d9084e9e7ba5509fbc4c0a615705a183e35601f5
Joost Rijneveld <joostrijneveld@gmail.com>
2012-03-31 11:56:01 +0200

    Initial commit

---
+++ ChefInterpreter/src/com/chef/interpreter/ChefInterpreter.java
@@ -0,0 +1,13 @@
+package com.chef.interpreter;
+
+public class ChefInterpreter {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+	}
+
+}
