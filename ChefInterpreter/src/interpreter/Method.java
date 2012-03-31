7446361dfe512d0be7e22d5ae58e13236e58b885
Joost Rijneveld <joostrijneveld@gmail.com>
2012-03-31 21:23:00 +0200

    Methoden worden nu met regex geparsed, waardoor 't veel beter werkt / overzichtelijker is

--- ChefInterpreter/src/interpreter/Method.java
+++ ChefInterpreter/src/interpreter/Method.java
@@ -1,5 +1,8 @@
 package interpreter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Method {
 
 	public enum Type {
@@ -34,9 +37,114 @@ public class Method {
 	public Type type;
 	
 	public Method(String line, int n) throws ChefException {
-		if (line.matches("^Take ([a-z][A-Z] )* from refrigerator.$")) {
-			
+		Matcher matcher;
+		matcher = Pattern.compile("^Take ([a-zA-Z ]+) from refrigerator.$").matcher(line); 
+		if (matcher.find()) {
+			ingredient = matcher.group(1);
+			type = Type.Take;
+			return;
 		}
+		matcher = Pattern.compile("^(Put|Fold) ([a-zA-Z ]+) into( (\\d+)(nd|th|st))? mixing bowl.$").matcher(line);
+		if (matcher.find()) {
+			type = matcher.group(1) == "Put" ? Type.Put : Type.Fold;
+			ingredient = matcher.group(2);
+			mixingbowl = matcher.group(4) == null ? 0 : Integer.parseInt(matcher.group(4));
+			return;
+		}
+		matcher = Pattern.compile("^Add dry ingredients( to( (\\d+)(nd|th|st))? mixing bowl)?.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.AddDry;
+			mixingbowl = matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3));
+			return;
+		}
+		matcher = Pattern.compile("^(Add|Remove|Combine|Divide) ([a-zA-Z ]+)( (to|into|from)( (\\d+)(nd|th|st))? mixing bowl)?.$").matcher(line);
+		if (matcher.find()) {
+			type = matcher.group(1) == "Add" ? Type.Add : (matcher.group(1) == "Remove" ? Type.Remove : (matcher.group(1) == "Combine" ? Type.Combine : Type.Divide));
+			ingredient = matcher.group(2);
+			mixingbowl = matcher.group(6) == null ? 0 : Integer.parseInt(matcher.group(6));
+			return;
+		}
+		matcher = Pattern.compile("^Liquefy contents of the( (\\d+)(nd|th|st))? mixing bowl.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.LiquefyBowl;
+			mixingbowl = matcher.group(2) == null ? 0 : Integer.parseInt(matcher.group(2));
+			return;
+		}
+		matcher = Pattern.compile("^Liquefy ([a-zA-Z ]+).$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Liquefy;
+			ingredient = matcher.group(1);
+			return;
+		}
+		//Stir [the [nth] mixing bowl] for number minutes.
+		matcher = Pattern.compile("^Stir( the( (\\d+)(nd|th|st))? mixing bowl)? for (\\d+) minutes.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Stir;
+			mixingbowl = matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3));
+			time = Integer.parseInt(matcher.group(5));
+			return;
+		}
+		//Stir ingredient into the [nth] mixing bowl.
+		matcher = Pattern.compile("^Stir ([a-zA-Z ]+) into the( (\\d+)(nd|th|st))? mixing bowl.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.StirInto;
+			ingredient = matcher.group(1);
+			mixingbowl = matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3));
+			return;
+		}
+		matcher = Pattern.compile("^Mix( the( (\\d+)(nd|th|st))? mixing bowl)? well.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Mix;
+			mixingbowl = matcher.group(3) == null ? 0 : Integer.parseInt(matcher.group(3));
+			return;
+		}
+		matcher = Pattern.compile("^Clean( (\\d+)(nd|th|st))? mixing bowl.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Clean;
+			mixingbowl = matcher.group(2) == null ? 0 : Integer.parseInt(matcher.group(2));
+			return;
+		}
+		matcher = Pattern.compile("^Pour contents of the( (\\d+)(nd|th|st))? mixing bowl into the( (\\d+)(nd|th|st))? baking dish.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Pour;
+			mixingbowl = matcher.group(2) == null ? 0 : Integer.parseInt(matcher.group(2));
+			bakingdish = matcher.group(5) == null ? 0 : Integer.parseInt(matcher.group(5));
+			return;
+		}
+		matcher = Pattern.compile("^Set aside.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.SetAside;
+			return;
+		}
+		matcher = Pattern.compile("^Refrigerate( for (\\d+) hours)?.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Refrigerate;
+			time = matcher.group(2) == null ? 0 : Integer.parseInt(matcher.group(2));
+			return;
+		}
+		matcher = Pattern.compile("^Serve with ([a-zA-Z ]+).$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Serve;
+			System.out.println(matcher.group(1));
+			auxrecipe = matcher.group(1);
+			return;
+		}
+		matcher = Pattern.compile("^([a-zA-Z]+)( the ([a-zA-Z ]+))? until ((?i)\\1)ed.$").matcher(line);
+		if (matcher.find()) {
+			type = Type.VerbUntil;
+			verb = matcher.group(1);
+			ingredient = matcher.group(3);
+			return;
+		}
+		matcher = Pattern.compile("^([a-zA-Z]+) the ([a-zA-Z ]+).$").matcher(line);
+		if (matcher.find()) {
+			type = Type.Verb;
+			verb = matcher.group(1);
+			ingredient = matcher.group(2);
+			return;
+		}
+		
+		
 	}
 	
 	/*public Method(String line, int n) throws ChefException {
